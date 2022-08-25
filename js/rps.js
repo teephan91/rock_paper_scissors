5d0eb6158d7774ad5a2dd0c94d8e14e9e72cb007
Thinh Phan <pseudothinhphan@gmail.com>
2022-08-24 19:34:58 -0700

    Create 'getComputerChoice' function
    This function randomly selects an element inside
    an array that consists of 'Rock', 'Paper', and
    'Scissors'.
    
    It's to make the computer's play in the game.

---
+++ js/rps.js
@@ -0,0 +1,6 @@
+function getComputerChoice() {
+    const computerChoice = ["Rock", "Paper", "Scissors"];
+    let randomArrayElement = Math.floor(Math.random() * computerChoice.length);
+    let randomComputerChoice = computerChoice[randomArrayElement];
+    return randomComputerChoice;
+}
