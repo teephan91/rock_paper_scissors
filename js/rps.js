7f665eb67fcd972350389d65bddaba88e0eaf2a7
Thinh Phan <pseudothinhphan@gmail.com>
2022-08-26 19:11:12 -0700

    Create 'playRound' function
    This function walks through a game of Rock,
    Paper, and Scissors. Player picks first then
    computer. After that, the function announces
    the winner.

--- js/rps.js
+++ js/rps.js
@@ -4,3 +4,27 @@ function getComputerChoice() {
     let randomComputerChoice = computerChoice[randomArrayElement];
     return randomComputerChoice;
 }
+
+function playRound(playerSelection, computerSelection) {
+    const win = `You won! ${playerSelection} beats ${computerSelection}.`;
+    const lose = `You lost! ${computerSelection} beats ${playerSelection}.`;
+    const tie = `It's a tie! ${playerSelection} ties ${computerSelection}.`;
+
+    if (playerSelection.toLowerCase() == "rock") {
+        return (computerSelection == "Scissors") ? win
+        : (computerSelection == "Paper") ? lose
+        : tie;
+    } else if (playerSelection.toLowerCase() == "paper") {
+        return (computerSelection == "Rock") ? win
+        : (computerSelection == "Scissors") ? lose
+        : tie;
+    } else if (playerSelection.toLowerCase() == "scissors") {
+        return (computerSelection == "Paper") ? win
+        : (computerSelection == "Rock") ? lose
+        : tie;
+    }
+}
+
+const playerSelection = prompt("Choose between Rock, Paper, or Scissors!");
+const computerSelection = getComputerChoice();
+alert(playRound(playerSelection, computerSelection));
