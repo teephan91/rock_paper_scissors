744d520a4a328508d1a11a80d5d42e6f2b583735
Thinh Phan <pseudothinhphan@gmail.com>
2022-08-27 19:00:38 -0700

    Create 'game' function
    This function calls the 'playRound' function
    to play a 5-round game. It also keeps score
    and announces the winner at the end.
    
    Tie game will not be counted toward the rounds.
    And whoever reaches 3 points first wins automatically.

--- js/rps.js
+++ js/rps.js
@@ -25,6 +25,37 @@ function playRound(playerSelection, computerSelection) {
     }
 }
 
-const playerSelection = prompt("Choose between Rock, Paper, or Scissors!");
-const computerSelection = getComputerChoice();
-alert(playRound(playerSelection, computerSelection));
+function game() {  
+    let playerResult = 0;
+    let computerResult = 0;
+
+    for (let i = 0; i < 5; i++) {
+        const playerSelection = prompt("Choose between Rock, Paper, or Scissors!");
+        const computerSelection = getComputerChoice();
+        const roundResult = playRound(playerSelection, computerSelection);
+
+        alert(roundResult);
+        
+        if (roundResult.includes("You won!")) {
+            playerResult++;
+        } else if (roundResult.includes("You lost!")) {
+            computerResult++;
+        } else if (roundResult.includes("It's a tie!")) {
+            playerResult += 0;
+            computerResult += 0;
+            i--;
+        }
+
+        if (playerResult == 3) {  
+            return alert("You won the game!");
+            break;
+        }
+
+        if (computerResult == 3) { 
+            return alert("You lost the game!");
+            break;
+        }
+    }
+}
+
+game();
