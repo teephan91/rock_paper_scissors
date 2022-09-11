122be52bc66b6f7b6db26fd68a4e3183c4411c81
Thinh Phan <pseudothinhphan@gmail.com>
2022-09-11 16:47:06 -0700

    Cmd+/ playGame & add event listener to new buttons

--- js/rps.js
+++ js/rps.js
@@ -11,51 +11,63 @@ function playRound(playerSelection, computerSelection) {
     const tie = `It's a tie! ${playerSelection} ties ${computerSelection}.`;
 
     if (playerSelection.toLowerCase() == "rock") {
-        return (computerSelection == "Scissors") ? win
-        : (computerSelection == "Paper") ? lose
-        : tie;
+        return (computerSelection == "Scissors") ? alert(win)
+        : (computerSelection == "Paper") ? alert(lose)
+        : alert(tie);
     } else if (playerSelection.toLowerCase() == "paper") {
-        return (computerSelection == "Rock") ? win
-        : (computerSelection == "Scissors") ? lose
-        : tie;
+        return (computerSelection == "Rock") ? alert(win)
+        : (computerSelection == "Scissors") ? alert(lose)
+        : alert(tie);
     } else if (playerSelection.toLowerCase() == "scissors") {
-        return (computerSelection == "Paper") ? win
-        : (computerSelection == "Rock") ? lose
-        : tie;
+        return (computerSelection == "Paper") ? alert(win)
+        : (computerSelection == "Rock") ? alert(lose)
+        : alert(tie);
     }
 }
 
-function playGame() {  
-    let playerResult = 0;
-    let computerResult = 0;
+const buttons = document.querySelectorAll('button');
+buttons.forEach((button) => {
+button.addEventListener('click', () => {
+    const playerSelection = button.textContent;
+        alert(`You pick ${playerSelection}`);
+    const computerSelection = getComputerChoice();
+        alert(`The computer picks ${computerSelection}`);
+    
+    playRound(playerSelection, computerSelection);
+});
+});
 
-    for (let i = 0; i < 5; i++) {
-        const playerSelection = prompt("Choose between Rock, Paper, or Scissors!");
-        const computerSelection = getComputerChoice();
-        const roundResult = playRound(playerSelection, computerSelection);
+// function playGame() {  
+//     let playerResult = 0;
+//     let computerResult = 0;
 
-        alert(roundResult);
+//     for (let i = 0; i < 5; i++) {
+//         const playerSelection = prompt("Choose between Rock, Paper, or Scissors!");
+//         const computerSelection = getComputerChoice();
+//         const roundResult = playRound(playerSelection, computerSelection);
+
+//         alert(roundResult);
         
-        if (roundResult.includes("You won!")) {
-            playerResult++;
-        } else if (roundResult.includes("You lost!")) {
-            computerResult++;
-        } else if (roundResult.includes("It's a tie!")) {
-            playerResult += 0;
-            computerResult += 0;
-            i--;
-        }
+//         if (roundResult.includes("You won!")) {
+//             playerResult++;
+//         } else if (roundResult.includes("You lost!")) {
+//             computerResult++;
+//         } else if (roundResult.includes("It's a tie!")) {
+//             playerResult += 0;
+//             computerResult += 0;
+//             i--;
+//         }
 
-        if (playerResult == 3) {  
-            return alert("You won the game!");
-            break;
-        }
+//         if (playerResult == 3) {  
+//             return alert("You won the game!");
+//             break;
+//         }
 
-        if (computerResult == 3) { 
-            return alert("You lost the game!");
-            break;
-        }
-    }
-}
+//         if (computerResult == 3) { 
+//             return alert("You lost the game!");
+//             break;
+//         }
+//     }
+// }
 
-playGame();
+// playGame();
